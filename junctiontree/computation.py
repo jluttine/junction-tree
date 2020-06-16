60621c137b41727848dfaded2a40c9cabf3e13df
Darryl Reeves <darryl.reeves@gmail.com>
2020-06-16 13:42:32 -0400

    Implemented Shafer-Shenoy

--- junctiontree/computation.py
+++ junctiontree/computation.py
@@ -115,11 +115,6 @@ def eliminate_variables(factors, variables, order, evidence={}):
         # perform einstein summation on the factors in the include group
         m_factor = sum_product.einsum(*args)
 
-        if m_factor.ndim  == 1:
-            # 1-dimensional factors require special treatment
-            m_factor = m_factor[:, np.newaxis]
-            output_var_indices += [...]
-
 
         # return a list with remaining factors(excluded and marginalized factor) and their variables
         # [ex_fac1, ex_fac1_vars, ex_fac2, ex_fac2_vars,..., new_fac, new_fac_vars]
@@ -130,7 +125,10 @@ def eliminate_variables(factors, variables, order, evidence={}):
                         [factors_and_vars[fac_ix], factors_and_vars[fac_ix + 1]]
                         for fac_ix in exclude_factors
                     ],
-                    [m_factor, output_var_indices]
+                    [m_factor, output_var_indices] if m_factor.ndim  != 1 else [
+                        # 1-dimensional factors require special treatment
+                        np.expand_dims(m_factor, axis=1), output_var_indices + [...]
+                    ]
                 )
         )
         # end of __run function
@@ -158,6 +156,286 @@ def eliminate_variables(factors, variables, order, evidence={}):
             np.squeeze(sum_product.einsum(*factors_and_vars, marg_vars)),
             [var for var in marg_vars if var != ...]
     )
+
+
+def compute_beliefs(tree, potentials, clique_vars):
+    ''' Computes beliefs for clique potentials in a junction tree
+    using Shafer-Shenoy updates.
+
+    :param tree: list representing the structure of the junction tree
+    :param potentials: list of numpy arrays for cliques in junction tree
+    :param clique_vars: list of variables included in each clique in potentials list
+    :return: list of numpy arrays defining computed beliefs of each clique
+    '''
+
+    def get_message(sepset_ix, tree, beliefs, clique_vars):
+        ''' Computes message with scope defined by sepset
+
+        :param sepset_ix: index of sepset scope in which to return message
+        (use slice(0) for no sepset)
+        :param tree: list representation of tree rooted by cluster for which message
+        will be computed
+        :param beliefs: list of numpy arrays for cliques in junction tree
+        :param clique_vars: list of variables included in each clique in potentials list
+        :return: message: potential with scope defined by sepset (or None if tree includes root)
+        '''
+
+        clique = clique_vars[tree[0]]
+
+        messages = [
+            comp # message or sepset variables in order processed
+            for sepset_ix, subtree in tree[1:]
+            for comp in [
+                get_message(sepset_ix, subtree, beliefs, clique_vars),
+                clique_vars[sepset_ix]
+            ]
+        ]
+
+        neighbor_vars = [
+            var
+            for ss_ix, subtree in tree[1:]
+            for var in clique_vars[ss_ix]
+        ]
+
+        neighbor_vars = list(set(neighbor_vars))
+
+        # reindex vars before doing einsum computation
+        var_map = {
+            var:i
+            for i, var in enumerate(list(set(
+                clique_vars[sepset_ix] + clique + neighbor_vars
+            )))
+        }
+
+        # map sepset vars using new indices
+        messages[1::2] = [
+            [
+                var_map[var]
+                for var in ss
+            ]
+            for ss in messages[1::2]
+        ]
+
+
+        m_neighbor_vars = [var_map[var] for var in neighbor_vars]
+
+        # multiply neighbor messages
+        messages = messages if len(messages) else [1]
+
+        msg_prod = sum_product.einsum(
+                                *messages,
+                                m_neighbor_vars
+        )
+
+        m_sepset = [var_map[var] for var in clique_vars[sepset_ix]]
+
+        args = [msg_prod, m_neighbor_vars] + [beliefs[tree[0]], [var_map[var] for var in clique], m_sepset]
+
+        # compute message as marginalization over non-sepset values
+        # multiplied by product of messages
+        # with output being vars in input sepset
+        message = sum_product.einsum(*args)
+
+
+        try:
+            # attempt to update belief
+            beliefs[sepset_ix] = message
+            return message
+        except TypeError:
+            # function called on full tree so no message to send
+            return None
+
+
+    def remove_message(msg_prod, prod_ixs, msg, msg_ixs, out_ixs):
+        ''' Removes (divides out) sepset message from
+            product of all neighbor sepset messages for a clique
+
+        :param msg_prod: product of all messages for clique
+        :param prod_ixs: variable indices in clique
+        :param msg: sepset message to be removed from product
+        :param msg_ixs: variable indices in sepset
+        :return: the product of messages with sepset msg removed (divided out)
+        '''
+
+        mask = np.in1d(prod_ixs, msg_ixs)
+
+        if all(mask):
+            # no need to expand indices for division (just align indices)
+
+            return (
+                prod_ixs,
+                np.divide(
+                    msg_prod,
+                    np.moveaxis(msg, msg_ixs, prod_ixs) if msg_ixs != prod_ixs else msg
+                )
+            )
+
+
+        # use mask to specify expanded dimensions in message
+        exp_ixs = np.full(msg_prod.ndim, None)
+        #exp_ixs[mask] = ...
+        exp_ixs[mask] = slice(None)
+
+        # use mask to select slice dimensions
+        mask = np.in1d(prod_ixs, out_ixs)
+        slice_ixs = np.full(msg_prod.ndim, slice(None))
+        slice_ixs[~mask] = 0
+
+        # create dummy dimensions for performing division (with exp_ix)
+        # slice out dimensions of sepset variables from division result (with slice_ix)
+
+        return (
+                np.array(prod_ixs)[mask].tolist(),
+                np.divide(
+                    msg_prod,
+                    msg[ tuple(exp_ixs) ]
+                )[ tuple(slice_ixs) ]
+        )
+
+
+    def send_message(message, sepset_ix, tree, beliefs, clique_vars):
+        ''' Sends message to cluster at root of tree
+
+        :param message: message sent by neighbor
+                (use np.array(1) for no message)
+        :param sepset_ix: index of sepset scope in which message sent
+                (use slice(0) for no sepset)
+        :param tree: list representation of tree rooted by cluster receiving message
+        :param beliefs: list of numpy arrays for cliques in junction tree
+        :param clique_vars: list of variables included in each clique in potentials list
+        '''
+
+        clique = clique_vars[tree[0]]
+
+        # computed messages stored in beliefs for neighbor sepsets
+
+        messages = [
+                comp
+                for ss_ix, _ in tree[1:]
+                for comp in [beliefs[ss_ix], clique_vars[ss_ix]]
+        # adding message sent
+        ] + [message, clique_vars[sepset_ix]]
+
+        all_neighbor_vars = [
+            var
+            for ss_ix, subtree in tree[1:]
+            for var in clique_vars[ss_ix]
+        ] + clique_vars[sepset_ix]
+
+        neighbor_vars = list(set(all_neighbor_vars))
+
+        # reindex vars before doing einsum computation
+        var_map = {
+            var:i
+            for i, var in enumerate(list(set(
+                clique_vars[sepset_ix] + clique + neighbor_vars
+            )))
+        }
+
+        # map sepset vars using new indices
+        messages[1::2] = [
+            [
+                var_map[var]
+                for var in ss
+            ]
+            for ss in messages[1::2]
+        ]
+
+        m_neighbor_vars = [var_map[var] for var in neighbor_vars]
+
+        # multiply neighbor messages
+        msg_prod = sum_product.einsum(
+                                *messages,
+                                m_neighbor_vars
+        )
+
+        m_clique_vars = [var_map[var] for var in clique]
+
+        # send message to each neighbor
+        ss_num = 0
+        for ss_ix, subtree in tree[1:]:
+
+            # sum over vars not in sepset
+            m_sepset = [var_map[var] for var in clique_vars[ss_ix]]
+
+            # divide product of messages by message for this neighbor
+            out_vars = set(
+                        [
+                            var
+                            for var in all_neighbor_vars
+                            if var not in clique_vars[ss_ix] or all_neighbor_vars.count(var) > 1
+                        ]
+            )
+
+            mask = np.in1d(m_neighbor_vars, [var_map[var] for var in out_vars])
+
+            mod_neighbor_vars = np.array(m_neighbor_vars)[mask]
+
+            '''mod_neighbor_vars, mod_msg_prod = remove_message(
+                                    msg_prod,
+                                    m_neighbor_vars,
+                                    beliefs[ss_ix],
+                                    m_sepset,
+                                    mod_neighbor_vars
+            ) if set(m_neighbor_vars) != set(m_sepset) else (m_neighbor_vars, np.ones_like(msg_prod))'''
+
+            mod_neighbor_vars, mod_msg_prod = remove_message(
+                                    msg_prod,
+                                    m_neighbor_vars,
+                                    beliefs[ss_ix],
+                                    m_sepset,
+                                    mod_neighbor_vars
+            )
+
+            args = [mod_msg_prod, mod_neighbor_vars] + [beliefs[tree[0]], m_clique_vars, m_sepset]
+            # calculate message to be sent
+            message = sum_product.einsum( *args )
+
+            # update sepset belief
+            beliefs[ss_ix] = beliefs[ss_ix] * message
+
+            send_message(message, ss_ix, subtree, beliefs, clique_vars)
+            ss_num += 1
+
+        # update belief for clique
+        args = [
+            beliefs[tree[0]],
+            m_clique_vars,
+            msg_prod,
+            m_neighbor_vars,
+            m_clique_vars
+        ]
+
+        beliefs[tree[0]] = sum_product.einsum(*args)
+
+
+    def __run(tree, beliefs, clique_vars):
+        ''' Collect messages from neighbors recursively. Then, send messages
+            recursively. Updated beliefs when this
+
+        :param tree: list representing the structure of the junction tree
+        :param briefs: list of numpy arrays for cliques in junction tree
+        :param clique_vars: list of variables included in each clique in potentials list
+        :return beliefs: consistent beliefs after Shafer-Shenoy updates applied
+        '''
+        # collect messages from neighbors (no child messages for leaf node)
+        # keep some order so messages are associated to neighbor
+        # a loop over cluster variables which makes recursive call at each
+        # iteration
+
+        # can we just use this function to kick-off get_message and send_message
+        # without doing any computations here?
+
+        # get messages from each neighbor
+        get_message(slice(0), tree, beliefs, clique_vars)
+
+        # send message to each neighbor
+        send_message(np.array(1), slice(0), tree, beliefs, clique_vars)
+
+        return beliefs
+
+    beliefs = [np.copy(p) for p in potentials]
+    return __run(tree, beliefs, clique_vars)
 
 
 sum_product = SumProduct(np.einsum)
--- tests/test_computation.py
+++ tests/test_computation.py
@@ -5,7 +5,7 @@
 
 from junctiontree import computation as comp
 import numpy as np
-from junctiontree.sum_product import SumProduct as sum_product
+import numbers
 
 def assert_junction_tree_equal(t1, t2):
     """Test equality of two junction trees
@@ -14,6 +14,7 @@ def assert_junction_tree_equal(t1, t2):
     Note this syntax supports separators in the trees because the separators
     just add levels.
     """
+
     # Equality of arrays
     np.testing.assert_allclose(t1[0], t2[0])
     # Equality of keys
@@ -25,43 +26,94 @@ def assert_junction_tree_equal(t1, t2):
     for (child_t1, child_t2) in zip(t1[2:], t2[2:]):
         assert_junction_tree_equal(child_t1, child_t2)
 
-def brute_force_sum_product(junction_tree):
-    """Compute brute force sum-product with einsum """
 
-    # Function to compute the sum-product with brute force einsum
-    arrays_keys = get_arrays_and_keys(junction_tree)
-    f = lambda output_keys: np.einsum(*(arrays_keys + [output_keys]))
+def get_factors_and_vars(factors, variables, evidence):
+    '''Creates a flat list of all factor arrays and lists of their variables
+    Output: [factor1, vars1, ..., factorN, varsN]
 
-    def __run(tree, f):
-        return [
-            f(tree[1]),
-            tree[1],
-            *[
-                __run(child_tree, f)
-                for child_tree in tree[2:]
-            ]
-        ]
+    :param factors: list of factors
+    :param variables: list of variables included in each factor in factors list
+    :param evidence: dictionary specifying (possible) observations for variables
+    :param factors_and_vars: list of interleaved factors and corresponding variable lists
+    '''
 
-    return __run(junction_tree, f)
 
-def get_arrays_and_keys(tree):
+    return sum(
+                [
+                    [
+                        # index array based on evidence value when evidence provided otherwise use full array
+                        fac[
+                            tuple(
+                                    [
+                                        slice(evidence.get(var, 0), evidence.get(var, fac.shape[i]) + 1)
+                                        for i, var in enumerate(vars)
+                                    ]
+                            )
+                        # workaround for scalar factors
+                        ] if not (isinstance(fac, numbers.Number)) else fac,
+                        vars
+                    ]
+                    for fac, vars in zip(factors, variables)
+                ],
+                []
+    )
+
+def get_arrays_and_keys(tree, node_list, potentials):
     """Get all arrays and their keys as a flat list
     Output: [array1, keys1, ..., arrayN, keysN]
     """
-    return list(tree[:2]) + sum(
+    return list([potentials[tree[0]],node_list[tree[0]]]) + sum(
         [
-            get_arrays_and_keys(child_tree)
-            for child_tree in tree[2:]
+            get_arrays_and_keys(child_tree, node_list, potentials)
+            for child_tree in tree[1:]
         ],
         []
     )
 
 
-def assert_sum_product(tree, value):
-    """ Test hugin vs brute force sum-product """
-    assert_junction_tree_equal(
-        brute_force_sum_product(tree),
-        [value, *tree[1:]]
+def brute_force_sum_product(tree, node_list, potentials):
+    """Compute brute force sum-product with einsum """
+
+    # Function to compute the sum-product with brute force einsum
+    factors_vars = get_arrays_and_keys(tree, node_list, potentials)
+    f = lambda output_vars: np.einsum(*(factors_vars + [output_vars]))
+
+    def __run(tree, node_list, p, f, res=[]):
+        res.append(f(node_list[tree[0]]))
+        for child_tree in tree[1:]:
+            __run(child_tree, node_list, p, f, res)
+        return res
+
+    return __run(tree, node_list, potentials, f)
+
+
+def assert_potentials_equal(p1, p2):
+    """Test equality of two potentials
+    """
+
+    # Same number of potentials
+    assert len(p1) == len(p2)
+
+    if len(p1):
+        # Check equality of arrays
+        np.testing.assert_allclose(p1[0], p2[0])
+        # recursively check remaining potentials
+        assert_potentials_equal(p1[1:], p2[1:])
+
+
+def assert_sum_product(tree, node_order, potentials, variables):
+    """ Test shafer-shenoy vs brute force sum-product """
+
+    # node_order represents the order nodes are traversed
+    # in get_factors_and_vars function
+
+    assert_potentials_equal(
+        brute_force_sum_product(
+                            tree,
+                            [variables[idx] for idx in node_order],
+                            [potentials[idx] for idx in node_order]
+        ),
+        comp.compute_beliefs(tree, potentials, variables)
     )
 
 
@@ -267,3 +319,330 @@ def test_variable_elimination_with_evidence():
                             m_factor_vars
                     )
     )
+
+
+def test_one_scalar_node():
+    assert_sum_product(
+                [
+                    0,
+                ],
+                [0],
+                [
+                    np.random.randn(),
+                ],
+                [[]] # no variables for scalar
+)
+
+def test_one_matrix_node():
+    assert_sum_product(
+                    [
+                        0,
+                    ],
+                    [0],
+                    [
+                        np.random.randn(2, 3),
+                    ],
+                    [
+                        [3,5]
+                    ]
+
+    )
+
+def test_one_child_node_with_all_variables_shared():
+    assert_sum_product(
+                    [
+                        0,
+                        (
+                            2,
+                            [
+                                1,
+                            ]
+                        )
+                    ],
+                    [0,2,1],
+                    [
+                        np.random.randn(2, 3),
+                        np.random.randn(3, 2),
+                        np.ones((3, 2)),
+                    ],
+                    [
+                        [3,5],
+                        [5,3],
+                        [5,3]
+                    ]
+    )
+
+def test_one_child_node_with_one_common_variable():
+    assert_sum_product(
+                    [
+                        0,
+                        (
+                            2,
+                            [
+                                1,
+                            ]
+                        )
+                    ],
+                    [0,2,1],
+                    [
+                        np.random.randn(2, 3),
+                        np.random.randn(3, 4),
+                        np.ones((3,)),
+                    ],
+                    [
+                        [3,5],
+                        [5,9],
+                        [5]
+                    ]
+    )
+
+def test_one_child_node_with_no_common_variable():
+    assert_sum_product(
+                    [
+                        0,
+                        (
+                            2,
+                            [
+                                1,
+                            ]
+                        )
+                    ],
+                    [0,2,1],
+                    [
+                        np.random.randn(2),
+                        np.random.randn(3),
+                        np.ones(()),
+                    ],
+                    [
+                        [3],
+                        [9],
+                        []
+                    ]
+
+    )
+
+def test_one_grand_child_node_with_no_variable_shared_with_grand_parent():
+    assert_sum_product(
+                    [
+                        0,
+                        (
+                            3,
+                            [
+                                1,
+                                (
+                                    4,
+                                    [
+                                        2,
+                                    ]
+                                )
+                            ]
+                        )
+                    ],
+                    [0,2,4,1,3],
+                    [
+                        np.random.randn(2, 3),
+                        np.random.randn(3, 4),
+                        np.random.randn(4, 5),
+                        np.ones((3,)),
+                        np.ones((4,)),
+                    ],
+                    [
+                        [3, 5],
+                        [5, 9],
+                        [9, 1],
+                        [5],
+                        [9]
+                    ]
+    )
+
+def test_one_grand_child_node_with_variable_shared_with_grand_parent():
+    assert_sum_product(
+                    [
+                        0,
+                        (
+                            3,
+                            [
+                                1,
+                                (
+                                    4,
+                                    [
+                                        2,
+                                    ]
+                                )
+                            ]
+                        )
+                    ],
+                    [0,2,4,1,3],
+                    [
+                        np.random.randn(2, 3),
+                        np.random.randn(3, 4),
+                        np.random.randn(6, 3),
+                        np.ones((3,)),
+                        np.ones((3,)),
+                    ],
+                    [
+                        [3, 5],
+                        [5, 9],
+                        [1, 5],
+                        [5],
+                        [5]
+                    ]
+    )
+
+def test_two_children_with_no_variable_shared():
+    assert_sum_product(
+                     [
+                        0,
+                        (
+                            3,
+                            [
+                                1,
+                            ]
+                        ),
+                        (
+                            4,
+                            [
+                                2,
+                            ]
+                        )
+                    ],
+                    [0,2,4,1,3],
+                    [
+                        np.random.randn(2, 3),
+                        np.random.randn(3, 4),
+                        np.random.randn(2, 5),
+                        np.ones((3,)),
+                        np.ones((2,)),
+                    ],
+                    [
+                        [3, 5],
+                        [5, 9],
+                        [3, 1],
+                        [5],
+                        [3]
+                    ]
+    )
+
+
+def test_two_child_with_shared_variable():
+    assert_sum_product(
+                     [
+                        0,
+                        (
+                            3,
+                            [
+                                1,
+                            ]
+                        ),
+                        (
+                            4,
+                            [
+                                2,
+                            ]
+                        )
+                    ],
+                    [0,2,4,1,3],
+                    [
+                        np.random.randn(2, 3),
+                        np.random.randn(3, 4),
+                        np.random.randn(3),
+                        np.ones((3,)),
+                        np.ones((3,)),
+
+                    ],
+                    [
+                        [3, 5],
+                        [5, 9],
+                        [5],
+                        [5],
+                        [5]
+                    ]
+    )
+
+def test_two_children_with_3D_tensors():
+    assert_sum_product(
+                    [
+                        0,
+                        (
+                            3,
+                            [
+                                1,
+                            ]
+                        ),
+                        (
+                            4,
+                            [
+                                2,
+                            ]
+                        )
+                    ],
+                    [0,2,4,1,3],
+                    [
+                        np.random.randn(2, 3, 4),
+                        np.random.randn(3, 4, 5),
+                        np.random.randn(3, 6),
+                        np.ones((3, 4)),
+                        np.ones((3,)),
+                    ],
+                    [
+                        [3,5,7],
+                        [5,7,9],
+                        [5,1],
+                        [5,7],
+                        [5]
+                    ]
+
+    )
+
+
+def test_divide_matrix_product():
+    # dividing messages from product when neighbor message is excluded
+    # this avoids re-doing einsum calculations to accomplish the same
+    # one full message product is calculated and messages are removed from the
+    # product by doing the divisioin operation
+
+    potentials = [
+                np.random.randn(2, 3, 6),
+                np.random.randn(3, 4),
+                np.random.randn(2, 5),
+                np.ones((3,)),
+                np.ones((2,)),
+                np.ones((6,)),
+                np.random.randn(4, 6)
+    ]
+
+    variables = [
+                [3, 5, 7],
+                [5, 9],
+                [3, 1],
+                [5],
+                [3],
+                [7],
+                [2, 7]
+    ]
+
+    msg1 = np.einsum(potentials[1], variables[1], variables[3])
+    msg2 = np.einsum(potentials[2], variables[2], variables[4])
+    msg3 = np.einsum(potentials[6], variables[6], variables[5])
+
+    msg_prod = np.einsum(msg1, variables[3], msg2, variables[4], msg3, variables[5], variables[0])
+
+    msg_prod_x6 = np.einsum(msg1, variables[3], msg2, variables[4], [3,5])
+    assert np.allclose(msg_prod_x6, np.divide(msg_prod, msg3[None, None, ...])[:,:,0])
+
+    mask = np.in1d(variables[0], variables[6])
+    exp_ix = np.full(msg_prod.ndim, None)
+    exp_ix[mask] = ...
+
+    slice_ix = np.full(msg_prod.ndim, slice(None))
+    slice_ix[mask] = 0
+
+    np.divide(msg_prod, msg3[tuple(exp_ix)])[tuple(slice_ix)]
+    assert np.allclose(msg_prod_x6, np.divide(msg_prod, msg3[tuple(exp_ix)])[tuple(slice_ix)])
+
+    msg_prod_x1 = np.einsum(msg2, variables[4], msg3, variables[-2], [3,7])
+    assert np.allclose(msg_prod_x1, np.divide(msg_prod, msg1[None, ..., None])[:,0,:])
+
+    msg_prod_x2 = np.einsum(msg1, variables[3], msg3, variables[5], [5,7])
+    assert np.allclose(msg_prod_x2, np.divide(msg_prod, msg2[..., None, None])[0,:,:])
+
