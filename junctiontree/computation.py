b92e56693b63262d3a0bc08d6c87b2779a52e0b1
Darryl Reeves <dar326@users.noreply.github.com>
2022-04-23 11:58:29 -0400

    direct implementation of Shafer-Shenoy updates

--- junctiontree/computation.py
+++ junctiontree/computation.py
@@ -1,5 +1,6 @@
 from junctiontree.sum_product import SumProduct
 import numpy as np
+import itertools
 
 # setting optimize to true allows einsum to benefit from speed up due to
 # contraction order optimization but at the cost of memory usage
@@ -56,6 +57,7 @@ def compute_beliefs(tree, potentials, clique_vars, dl=sum_product):
         :return: message: potential with scope defined by sepset (or None if tree includes root)
         '''
 
+        cluster_ix = tree[0]
         messages = [
             comp # message or sepset variables in order processed
             for sepset_ix, subtree in tree[1:]
@@ -65,35 +67,30 @@ def compute_beliefs(tree, potentials, clique_vars, dl=sum_product):
             ]
         ]
 
-        neighbor_vars = [
-            var
-            for ss_ix, subtree in tree[1:]
-            for var in clique_vars[ss_ix]
-        ]
+        # multiply neighbor messages
+        #messages = messages if len(messages) else [1,[]]
 
-        neighbor_vars = np.unique(neighbor_vars)
-
-        # multiply neighbor messages        
-        msg_prod = dl.einsum(*messages, neighbor_vars)
-
-        args = [msg_prod, neighbor_vars] + [beliefs[tree[0]], clique_vars[tree[0]], clique_vars[sepset_ix]]
+        args = messages + [beliefs[cluster_ix], clique_vars[cluster_ix], clique_vars[cluster_ix]]
 
         # compute message as marginalization over non-sepset values
         # multiplied by product of messages with output being vars in input sepset
 
-        message = dl.einsum(*args)
-
+        #update clique belief
+        beliefs[cluster_ix] = dl.einsum(*args)
 
         try:
-            # attempt to update belief
-            beliefs[sepset_ix] = message
-            return message
+            # attempt to update sepset belief
+            args = [beliefs[cluster_ix], clique_vars[cluster_ix], clique_vars[sepset_ix]]
+            beliefs[sepset_ix] = dl.einsum(*args)
+
+            # send sepset belief as message
+            return beliefs[sepset_ix]
         except TypeError:
             # function called on full tree so no message to send
             return None
 
 
-    def send_message(message, sepset_ix, tree, beliefs, clique_vars):
+    def send_message(message, sepset_ix, tree, beliefs, pots, clique_vars):
         '''Sends message from clique at root of tree
 
         :param message: message sent by neighbor
@@ -101,10 +98,12 @@ def compute_beliefs(tree, potentials, clique_vars, dl=sum_product):
         :param sepset_ix: index of sepset scope in which message sent
                 (use slice(0) for no sepset)
         :param tree: list representation of tree rooted by cluster receiving message
-        :param beliefs: list of numpy arrays for cliques in junction tree
+        :param beliefs: beliefs to update for cliques in junction tree
+        :param pots: list of original numpy arrays for cliques in junction tree
         :param clique_vars: list of variables included in each clique in potentials list
         '''
 
+        cluster_ix = tree[0]
         # computed messages stored in beliefs for neighbor sepsets
         messages = [
                 comp
@@ -113,79 +112,61 @@ def compute_beliefs(tree, potentials, clique_vars, dl=sum_product):
         # adding message sent
         ] + [message, clique_vars[sepset_ix]]
 
-
-        all_neighbor_vars = [
-            var
-            for vars in messages[1::2]
-            for var in vars
-        ]
-
-        neighbor_vars = np.unique(all_neighbor_vars)
-
-        # multiply neighbor messages
-        msg_prod = dl.einsum(
-                                *messages,
-                                neighbor_vars
-        )
-
         # send message to each neighbor
-        ss_num = 0
         for ss_ix, subtree in tree[1:]:
 
-            # remove sepset ix vars from neighbor vars
-            mod_neighbor_vars = np.setdiff1d(neighbor_vars, clique_vars[ss_ix])
-
-
-
-
-            # create product of messages that excludes the message from this sepset
+            # collect all messages (excluding those from ss_ix)
+            # using id() as sepset variables can be same even when sepsets are unique
             mod_messages = [
                                 comp
                                 for i in range(1,len(messages), 2)
-                                for comp in messages[i-1:i+1] if messages[i] != clique_vars[ss_ix]
+                                for comp in messages[i-1:i+1] if id(messages[i]) != id(clique_vars[ss_ix])
             ]
-            args = [dl.einsum(*mod_messages, mod_neighbor_vars), mod_neighbor_vars] + [beliefs[tree[0]], clique_vars[tree[0]], clique_vars[ss_ix]]
 
             # calculate message to be sent
-            message = dl.einsum( *args )
+            args = mod_messages + [pots[cluster_ix], clique_vars[cluster_ix], clique_vars[ss_ix]]
+            msg = dl.einsum( *args )
 
             # update sepset belief
-            beliefs[ss_ix] *= message
+            args = [beliefs[ss_ix], clique_vars[ss_ix], msg, clique_vars[ss_ix], clique_vars[ss_ix]]
+            beliefs[ss_ix] = dl.einsum(*args)
 
-            send_message(message, ss_ix, subtree, beliefs, clique_vars)
-            ss_num += 1
+            # send message to neighbor (excludes message from subtree)
+            send_message(msg, ss_ix, subtree, beliefs, pots, clique_vars)
+
 
         # update belief for clique
         args = [
-            beliefs[tree[0]],
-            clique_vars[tree[0]],
-            msg_prod,
-            neighbor_vars,
-            clique_vars[tree[0]]
+            beliefs[cluster_ix],
+            clique_vars[cluster_ix],
+            message,
+            clique_vars[sepset_ix],
+            clique_vars[cluster_ix]
         ]
 
+        beliefs[cluster_ix] = dl.einsum(*args)
 
-        beliefs[tree[0]] = dl.einsum(*args)
 
-
-    def __run(tree, beliefs, clique_vars):
+    def __run(tree, potentials, clique_vars):
         '''Collect messages from neighbors recursively. Then, send messages
         recursively. Updated beliefs when this
 
         :param tree: list representing the structure of the junction tree
-        :param briefs: list of numpy arrays for cliques in junction tree
+        :param potentials: list of numpy arrays for cliques in junction tree
         :param clique_vars: list of variables included in each clique in potentials list
         :return beliefs: consistent beliefs after Shafer-Shenoy updates applied
         '''
+
+        beliefs = [np.copy(p) for p in potentials]
 
         # get messages from each neighbor
         get_message(slice(0), tree, beliefs, clique_vars)
 
         # send message to each neighbor
-        send_message(np.array(1), slice(0), tree, beliefs, clique_vars)
+        send_message(np.array(1), slice(0), tree, beliefs, potentials, clique_vars)
+
 
         return beliefs
 
 
-    beliefs = [np.copy(p) for p in potentials]
-    return __run(tree, beliefs, clique_vars)
+    return __run(tree, potentials, clique_vars)
